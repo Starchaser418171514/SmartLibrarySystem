b4b9f89819758e2888fb4c7d516fcde7534cde5f
Jayden Wan Kar Ming-2025 <111164351+Starchaser418171514@users.noreply.github.com>
2026-05-09 17:02:33 +0800

    Create LibraryInterface for library operations

---
+++ Library/LibraryInterface.java
@@ -0,0 +1,8 @@
+package assignments.SmartLibrarySystem.Library;
+
+public interface LibraryInterface {
+    public void addBook(int isbn, String title, String author);
+    public void borrowBook(int isbn);
+    public void viewLatestHistory();
+    public void findBook(int isbn);
+}
