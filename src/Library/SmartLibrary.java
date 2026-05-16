07d3f9b23e6a064519652fa021a2171c68738731
Jayden Wan Kar Ming-2025 <111164351+Starchaser418171514@users.noreply.github.com>
2026-05-16 14:50:42 +0800

    Add files via upload

---
+++ src/Library/SmartLibrary.java
@@ -0,0 +1,51 @@
+package SmartLibrarySystem.src.Library;
+
+import SmartLibrarySystem.src.CatalogueArchitect.*;
+
+public class SmartLibrary implements LibraryInterface {
+    private LibraryCatalogue catalogue;
+    private BorrowStack borrowHistory;
+
+    /**
+     * Function to add a book to the library's catalogue
+     * 
+     * @param isbn   The unique ISBN number of the book
+     * @param title  The title of the book
+     * @param author The author of the book
+     */
+    public void addBook(int isbn, String title, String author) {
+        catalogue.addBook(isbn, title, author);
+    }
+
+    /**
+     * Function to borrow a book from the library
+     * 
+     * @param isbn The unique ISBN number of the book to borrow
+     * @return true if the book was successfully borrowed, false otherwise
+     */
+    public boolean borrowBook(int isbn) {
+        Book book = findBook(isbn);
+        if (book != null) {
+            borrowHistory.addBorrowedBook(book);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Function to view the latest borrowing history
+     */
+    public void viewLatestHistory() {
+        borrowHistory.showHistory();
+    }
+
+    /**
+     * Function to find a book in the library's catalogue
+     * 
+     * @param isbn The unique ISBN number of the book to find
+     * @return The Book object if found, null otherwise
+     */
+    public Book findBook(int isbn) {
+        return catalogue.findBook(isbn);
+    }
+}
